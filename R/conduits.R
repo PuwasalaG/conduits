69be5ce6ef2b18d79288b115489f5334ef9f982e
PuwasalaG <puwasala.gamakumara@gmail.com>
2020-12-10 14:20:16 +1100

    Adding a help page to the package

---
+++ R/conduits.R
@@ -0,0 +1,8 @@
+#' conduits: CONDitional User Interface for Time Series normalisation
+#' @description Methods and tools for conditional normalisation of time series using additive models.
+#' This includes functions to estimate conditional autocorrelation functions and conditional cross-correlation functions.
+#' Further these functions can be used to estimate river flow time between two sensor locations in a river system.
+#' @author Puwasala Gamakumara
+#' @docType package
+#' @name  conduits
+NULL
